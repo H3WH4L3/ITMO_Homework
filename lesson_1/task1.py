5371de9927e670ee087774acc88f11207bc65c70
Никита <giveyourcatsomemilk@outlook.com>
2025-09-30 13:10:37 +0300

    Task_01 is Done

--- lesson_1/task1.py
+++ lesson_1/task1.py
@@ -1,7 +1,15 @@
 # todo: Определить в коде переменные:
 # 1. Целочисленного типа
+x = 10
 # 2. Вещественного типа
+y = 1.2
 # 3. Логического типа
+flag = True
 # 4. Строкового типа
+s = "Some string"
 # 5. Пустого типа
+empty = None
 # Вывести их типы (надо погуглить)
+
+for element in [x, y, flag, s, empty]:
+    print(f'{element} type: {type(element)}')
