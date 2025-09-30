e7c3279a61a59b4fcf3aa9bf51603a43acdbfc7f
Никита <giveyourcatsomemilk@outlook.com>
2025-09-30 14:48:10 +0300

    Task_07 is Done

--- lesson_1/task7.py
+++ lesson_1/task7.py
@@ -1,2 +1,18 @@
 #todo: Даны три точки A , B , C на числовой оси. Найти длины отрезков AC и BC и их сумму.
 # Примечание: все точки получаем через функцию input().
+
+A, B, C = map(int, input().split())
+
+# Длина отрезка AC:
+print("Длина отрезка AC: ", end="")
+segment_AC = abs(A - C)
+print(segment_AC)
+
+# Длина отрезка BC:
+print("Длина отрезка BC: ", end="")
+segment_BC = abs(B - C)
+print(segment_BC)
+
+# Сумма отрезков
+print("Сумма отрезков AC и BC: ", end="")
+print(segment_AC + segment_BC)
