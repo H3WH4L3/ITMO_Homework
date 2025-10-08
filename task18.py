7bb2bb811d7f9423cd77364df3b46584c84825c5
Никита <giveyourcatsomemilk@outlook.com>
2025-10-08 13:28:07 +0300

    Task 18 is Done

--- task18.py
+++ task18.py
@@ -5,4 +5,7 @@ readers_books = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1' }
 #Даны читатели газет
 readers_magazines = { 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
 
-Найти пользователей кто читает и книги и газеты
+# Найти пользователей кто читает и книги и газеты
+
+readers_both = readers_books.intersection(readers_magazines)
+print(readers_both)
