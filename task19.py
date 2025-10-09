137030dfa926177aa445d6f68dcb07cfb1750cab
Никита <giveyourcatsomemilk@outlook.com>
2025-10-09 13:58:47 +0300

    Task 19 is Done

---
+++ task19.py
@@ -0,0 +1,17 @@
+#todo: Требуется создать csv-файл «algoritm.csv» со следующими столбцами:
+# id) - номер по порядку (от 1 до 10);
+# значение из списка algoritm
+
+algoritm = [ "C4.5" , "k - means" , "Метод опорных векторов" ,
+             "Apriori", "EM", "PageRank" , "AdaBoost", "kNN" ,
+             "Наивный байесовский классификатор", "CART" ]
+
+# Каждое значение из списка должно находится на отдельной строке.
+# Пример файла algoritm.csv:
+# 1) "C4.5"
+# 2) "k - means"
+# .....
+
+with open("algoritm.csv", "w", encoding="UTF-8") as f:
+    for i, value in enumerate(algoritm, start=1):
+        f.write(f'{i}) "{value}"\n')
---
+++ task23.py
@@ -0,0 +1,5 @@
+# todo: Исправить ошибку в коде игры  ../code/ya_kube.py
+
+from lesson_5.code import ya_kube
+
+
