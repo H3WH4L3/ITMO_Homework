f5b2b1f11727043c2fe32862068f33cd5f1d920e
Никита <giveyourcatsomemilk@outlook.com>
2025-10-09 14:20:09 +0300

    Task 20 is Done

--- task20.py
+++ task20.py
@@ -1,14 +1,26 @@
 #todo: Выведите все строки данного файла в обратном порядке, допишите их в этот же файл.
 # Для этого считайте список всех строк при помощи метода readlines().
 
-#Содержимое файла inverted_sort.txt
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
+# #Содержимое файла inverted_sort.txt
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
 
-# Результат
-Complex is better than complicated.
-Simple is better than complex.
-Explicit is better than implicit.
-Beautiful is better than ugly.
+# # Результат
+# Complex is better than complicated.
+# Simple is better than complex.
+# Explicit is better than implicit.
+# Beautiful is better than ugly.
+
+with open("inverted_sort.txt", mode="a+", encoding="UTF-8") as f:
+    f.seek(0)
+    text = f.readlines()
+    
+    if text and not text[-1].endswith("\n"):
+        f.write("\n")
+
+    for line in text[::-1]:
+        if not line.endswith("\n"):
+            line += "\n"
+        f.write(line)
