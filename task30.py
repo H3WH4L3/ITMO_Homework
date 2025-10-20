98fa4f36e555bb0fe943d7934d9a8cd078c94fad
Никита <giveyourcatsomemilk@outlook.com>
2025-10-20 11:02:25 +0300

    Task 30 is Done

--- task30.py
+++ task30.py
@@ -3,3 +3,13 @@
 # Все цены переведите в рубли. Задачу следует решить с использованием списковых включений.
 
 prices = ["₽1500", "20.50 USD", "invalid", "€25.00", "$15.99", "18.99", "N/A", "¥5000"]
+
+rates = {"USD": 80.98, "$": 80.98, "€": 94.58, "¥": 0.53, "₽": 1}
+
+result = [
+    float("".join([ch for ch in price if ch.isdigit() or ch == "."])) * rate
+    for price in prices
+    for sym, rate in rates.items()
+    if sym in price
+]
+print(*[f"{value} рублей" for value in result], sep="\n")
